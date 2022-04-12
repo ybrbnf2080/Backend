0178dc0b8b9c8c2e48ab823c60585e8bf047845a
kritinidzin <danil.kritsin@gmail.com>
2022-04-12 21:14:50 +0300

    fix connection

--- src/WeGo/settings.py
+++ src/WeGo/settings.py
@@ -2,7 +2,7 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    server_host: str = "127.0.0.1"
+    server_host: str = "0.0.0.0"
     server_port: int = 8080
     db_url: str = "postgresql://hostman:b9392b97@94.228.113.90:5434/database"
 
