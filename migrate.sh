30bdd1d06470fa88182425344d3092e777083cca
ybrbnf2080 <nekitagundorov87006@gmail.com>
2022-04-18 01:13:33 +0400

    test

--- migrate.sh
+++ migrate.sh
@@ -1,6 +1,6 @@
 # /bin/bash
 
-docker exec -i api /bin/bash -c "alembic stamp head"
+#docker exec -i api /bin/bash -c "alembic stamp head"
 docker exec -i api /bin/bash -c "alembic revision --message='Update migrate in github action' --autogenerate"
 docker exec -i api /bin/bash -c "alembic upgrade head"
 
